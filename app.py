e0e3bcf55adaedfe9e7541f758faeae6564b4b9a
Grindpa <anyanwufrancischu@yahoo.com>
2025-11-16 08:34:12 +0100

    started the index page and js

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, jsonify
 from flask_session import Session
 from cs50 import SQL
 
@@ -8,18 +8,31 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/")
+@app.context_processor
+def ready():
+    if session.get("cancel"):
+        x = "cancel"
+        return dict(x = x)
+    else:
+        x = ""
+        return dict(x = x)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        session["cancel"] = request.json["q"]
+        print(request.json["q"])
+        return jsonify({"msg": "cancel session saved"})
     if not session.get("enter"):
         return redirect("/landing")
-    return render_template("index.html")
+    return render_template("index.html", page_id="index")
 
 @app.route("/landing", methods=["GET", "POST"])
 def landing():
     if request.method == "POST":
-        session["enter"] = request.form.get("enter")
+        session["enter"] = request.form.get("name")
         return redirect("/")
-    return render_template("landing.html")
+    return render_template("landing.html", page_id = "landing")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, use_reloader=True, reloader_type="watchdog")
--- static/script.js
+++ static/script.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Script loaded and running!');
+    if (document.body.id === "index") {
+        const hi = document.querySelector(".hi");
+        const ready = hi.querySelectorAll(".ready");
+        const cl = hi.querySelector("#cl");
+
+        for (let i=0; i < ready.length; i++) {
+            ready[i].addEventListener("click", async () => {
+                hi.style.display = "none";
+
+                let r = await fetch("/", {
+                    method: "post",
+                    body: JSON.stringify({"q": "cancel"})
+                })
+                let data = await r.json()
+                console.log(data)
+            });
+        };
+    }
 });
